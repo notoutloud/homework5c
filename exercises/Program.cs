632ffde4eee59cc4987d4ae090e92e5dd338eced
Kirill <kirill.chukanov@gmail.com>
2023-07-20 14:59:05 +0300

    initial

---
+++ exercises/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace exercises;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        void FillArray(int[] array)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.Next(-9, 10);
+                }
+            }
+
+        void Print(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]}  ");
+                }
+                Console.WriteLine();
+            }
+            
+    }
+}

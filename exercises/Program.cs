6be573d5abc7a0df65fd2324680cc1cc99937bfb
Kirill <kirill.chukanov@gmail.com>
2023-07-20 15:25:15 +0300

    ease

--- exercises/Program.cs
+++ exercises/Program.cs
@@ -4,23 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        void FillArray(int[] array)
-            {
+        int Input(string text){
+            Console.Write(text);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        void FillArray(int[] array){
+
                 Random rnd = new Random();
                 for (int i = 0; i < array.Length; i++)
                 {
-                    array[i] = rnd.Next(-9, 10);
+                    array[i] = rnd.Next(100, 1000);
                 }
             }
 
-        void Print(int[] array)
-            {
+        void Print(int[] array){
                 for (int i = 0; i < array.Length; i++)
                 {
                     Console.Write($"{array[i]}  ");
                 }
                 Console.WriteLine();
             }
-            
+        void Task34(){
+                int size = 5;
+                
+            }
     }
 }
